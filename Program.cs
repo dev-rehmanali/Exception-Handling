0db6cf5c653238e8a08d362b429bb64b1ac3fd3d
dev.rehmanali <82761490+dev-rehmanali@users.noreply.github.com>
2021-09-28 15:53:54 +0500

    exception handling with report

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  
+             *  This code generates NullReferenceException
+             *  and generates warning before running
+             *  
+             *  int value = Int32.Parse(args[0]);
+              List<String> names;
+
+              if (value > 0)
+                  names = new List<String>();
+
+              names.Add("Major Major Major");
+            */
+
+            /* This code handles NullReferenceException */
+
+                List<String> names = new List<String>();
+                names.Add("Major Major Major");
+                        
+
+            }
+        }
+
+
+
+
+}
